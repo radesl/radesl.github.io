5932276f866dd1c1aed44b077743f725e707bee2
Radomir Slowik <radesl@interia.eu>
2017-10-26 16:40:11 +0200

    pobierz dane v2

--- jquery-pobierzDane/js/main.js
+++ jquery-pobierzDane/js/main.js
@@ -1,18 +1,12 @@
 $(document).ready(function () {
     $('button').click(function () {
         $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (response) {
-            /*var poj = document.createElement('div');
-            poj.setAttribute('id', 'dane-programisty');
-            document.body.appendChild(poj);*/
-            $('body').append('<div></div>');
-            $('div').attr('id', 'dane-programisty');
+            if ($('#dane-programisty').length < 1) {
+                $('body').append('<div></div>');
+                $('div').attr('id', 'dane-programisty');
+            }
             $.each(response, function (index, element) {
-                /*console.log(index + ': ' + element);
-                var dane = document.createElement('p');
-                dane.innerText = index + ': ' + element;
-                document.getElementById('dane-programisty').appendChild(dane);*/
                 $('#dane-programisty').append('<p>' + index + ': ' + element + '</p>');
-
             });
         });
     });
